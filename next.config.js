c84e7147650000930004ffdd950863e42e8f56e2
marcellino-dev <marcelinosouza.dev@gmail.com>
2026-04-21 12:21:57 -0300

    fix: CSP headers para MercadoPago

--- next.config.js
+++ next.config.js
@@ -10,6 +10,26 @@ const nextConfig = {
   experimental: {
     serverActions: { allowedOrigins: ['localhost:3000'] },
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.mercadopago.com https://*.mercadolibre.com https://*.mercadopago.com.br",
+              "frame-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://*.mercadopago.com.br",
+              "connect-src 'self' https://*.mercadopago.com https://*.mercadolibre.com https://*.mercadopago.com.br",
+              "img-src 'self' data: https://*.mercadopago.com https://*.mercadolibre.com",
+              "style-src 'self' 'unsafe-inline'",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
